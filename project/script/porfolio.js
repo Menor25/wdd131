1166807644b2d482bdfdc5fc1896f0c8e546b491
Theophilus Ajiri Menor <ajirimenor@gmail.com>
2025-12-14 12:01:35 +0100

    Contact page and errors corrected in home page

--- project/script/porfolio.js
+++ project/script/porfolio.js
@@ -9,3 +9,8 @@ navToggle.addEventListener('click', () => {
     
     navToggle.classList.toggle('is-active');
 });
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear
+
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`
